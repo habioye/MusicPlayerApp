8815c7b982c9ecefa863a9c66da0a3074aded731
habioye <78454514+habioye@users.noreply.github.com>
2024-07-08 01:58:26 -0400

    added constructor method

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -1,24 +1,36 @@
 import { Component } from '@angular/core';
 import { MatSlider } from '@angular/material/slider';
-import { AudioService } from "../../services/audio.service";
-import { CloudService } from "../../services/cloud.service";
-import { StreamState } from "../../../interfaces/stream-state";
+import { AudioService } from '../../services/audio.service';
+import { CloudService } from '../../services/cloud.service';
+import { StreamState } from '../../../interfaces/stream-state';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrl: './player.component.scss'
+  styleUrl: './player.component.scss',
 })
 // src/app/pages/player/player.component.ts
 // ... import statements and @Component declaration ...
 export class PlayerComponent {
   files: Array<any> = [
-    { name: "First Song", artist: "Inder" },
-    { name: "Second Song", artist: "You" }
+    { name: 'First Song', artist: 'Inder' },
+    { name: 'Second Song', artist: 'You' },
   ];
   // state:MatSlider = new MatSlider();
   state: StreamState;
   currentFile: any = {};
+
+  constructor( public audioService: AudioService, public cloudService: CloudService) {
+    // get media files
+    cloudService.getFiles().subscribe((files) => {
+      this.files = files;
+    });
+
+    // listen to stream state
+    this.audioService.getState().subscribe((state) => {
+      this.state = state;
+    });
+  }
 
   isFirstPlaying() {
     return false;
@@ -26,20 +38,9 @@ export class PlayerComponent {
   isLastPlaying() {
     return true;
   }
-  play() {
-
-  }
-  pause() {
-
-  }
-  next() {
-
-  }
-  previous() {
-
-  }
-  onSliderChangeEnd(input:any) {
-
-  }
+  play() { }
+  pause() { }
+  next() { }
+  previous() { }
+  onSliderChangeEnd(input: any) { }
 }
-
